f4c8d226614dc44208a3cdd5478af3157f5cd259
lausnchez <lausnchezmartn@gmail.com>
2026-03-16 17:18:53 +0100

    Hero terminado

--- Gulp+Bootstrap5(2025)/build/js/main.js
+++ Gulp+Bootstrap5(2025)/build/js/main.js
@@ -10778,3 +10778,19 @@ function showLanguageMobileSubmenu(){
     // Mostrar menú
     headerLanguageMenu?.classList.toggle(classToggle);
 }
+// Recoger los elementos del Hero y variables de pantalla
+const hero_header = document.querySelector(".hero__contents h2");
+const mobileVariable = getComputedStyle(document.documentElement).getPropertyValue("--tablet");
+const mobileScreenWidth = parseInt(mobileVariable);
+
+// Cambiar de clase según el tamaño de la ventana
+function updateLayout() {
+  if (window.innerWidth <= mobileScreenWidth) {
+    hero_header.classList.replace("base_title-hero", "base_header2-hero");
+  } else {
+    hero_header.classList.replace("base_header2-hero", "base_title-hero");
+  }
+}
+
+updateLayout();
+window.addEventListener("resize", updateLayout);
