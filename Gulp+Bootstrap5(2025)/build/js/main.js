1e1b66f3fc753bdbf85c243aa741fe5f306b7b2f
lausnchez <lausnchezmartn@gmail.com>
2026-03-17 14:40:52 +0100

    Terminar de limpiar variables

--- Gulp+Bootstrap5(2025)/build/js/main.js
+++ Gulp+Bootstrap5(2025)/build/js/main.js
@@ -10704,6 +10704,8 @@ return jQuery;
 } );
 // Recoger elementos del DOM
 //---------------------------------------------------------------------------------
+const body = document.querySelector("body");    // Para togglear el bloqueo del scroll
+
 // Elementos del profile
 const headerProfileButton = document.querySelector('#header_submenu__button_profile button');
 const headerProfileMenu = document.querySelector('.header__submenus__mobile .profile-dropdown');
@@ -10721,6 +10723,7 @@ const headerLanguageMenu = document.querySelector(".header__mobile-burger-submen
 
 const classToggle = "mobile-active";    // Despliega un menú
 const hiddenClass = "header-submenu-button-hidden"; // Oculta un elemento
+const bodyScrollBlock = "menu-open";    // Bloquea el scroll del body
 
 // Ocultar el botón de cierre al inicializar
 headerCloseButton.classList.add(hiddenClass);
@@ -10739,6 +10742,9 @@ function closeAllMenus(){
     headerProfileMenu?.classList.remove(classToggle);
     headerBurgerMenu?.classList.remove(classToggle);
     headerLanguageMenu?.classList.remove(classToggle);
+
+    // Habilitar de nuevo el scroll
+    body.classList.remove(bodyScrollBlock);
 }
 
 // Función para mostrar/ocultar el submenu del profile
@@ -10754,6 +10760,9 @@ function showProfileMobileSubmenu(){
     headerBurgerButton.classList.add(hiddenClass);
     headerCloseButton.classList.remove(hiddenClass);
 
+    // Bloquear scroll del body
+    body.classList.add(bodyScrollBlock);
+
 }
 
 // Función para mostrar/ocultar el submenu burger
@@ -10768,6 +10777,9 @@ function showBurgerMobileSubmenu(){
     headerProfileButton.classList.add(hiddenClass);
     headerBurgerButton.classList.add(hiddenClass);
     headerCloseButton.classList.remove(hiddenClass);
+
+    // Bloquear scroll del body
+    body.classList.add(bodyScrollBlock);
 }
 
 // Función para mostrar/ocultar el submenu language
