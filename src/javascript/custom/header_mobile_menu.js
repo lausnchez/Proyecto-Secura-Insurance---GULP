36445b452fbb7090c40f305c868d72c79c52e2b6
lausnchez <lausnchezmartn@gmail.com>
2026-03-24 12:15:09 +0100

    Info de la página de renovaciones

--- src/javascript/custom/header_mobile_menu.js
+++ src/javascript/custom/header_mobile_menu.js
@@ -5,21 +5,35 @@
 const body = document.querySelector("body");
 
 // Elementos del profile
-const headerProfileButton = document.querySelector('#header_submenu__button_profile button');
-const headerProfileMenu = document.querySelector('.header__submenus__mobile .profile-dropdown');
+const headerProfileButton = document.querySelector(
+  "#header_submenu__button_profile button",
+);
+const headerProfileMenu = document.querySelector(
+  ".header__submenus__mobile .profile-dropdown",
+);
 
 // Elementos del burger
-const headerBurgerButton = document.querySelector('#header_submenu__button_burger button');
-const headerBurgerMenu = document.querySelector('.header__submenus__mobile .header__mobile-burger-submenu');
+const headerBurgerButton = document.querySelector(
+  "#header_submenu__button_burger button",
+);
+const headerBurgerMenu = document.querySelector(
+  ".header__submenus__mobile .header__mobile-burger-submenu",
+);
 
 // Elementos del close
-const headerCloseButton = document.querySelector('#header_submenu__button_close');
+const headerCloseButton = document.querySelector(
+  "#header_submenu__button_close",
+);
 
 // Elementos del language
-const headerLanguageButton = document.querySelector(".header__mobile-burger-submenu__language-dropdown a");
-const headerLanguageMenu = document.querySelector(".header__mobile-burger-submenu__language-dropdown ul");
+const headerLanguageButton = document.querySelector(
+  ".header__mobile-burger-submenu__language-dropdown a",
+);
+const headerLanguageMenu = document.querySelector(
+  ".header__mobile-burger-submenu__language-dropdown ul",
+);
 
-const classToggle = "mobile-active";    // Despliega un menú
+const classToggle = "mobile-active"; // Despliega un menú
 const hiddenClass = "header-submenu-button-hidden"; // Oculta un elemento
 const notScroll = "not-scroll"; // Clase para evitar el scroll en mobile
 
@@ -28,59 +42,60 @@ headerCloseButton.classList.add(hiddenClass);
 
 // Función para cerrar el menú desplegado actualmente
 //---------------------------------------------------------------------------------
-headerCloseButton?.addEventListener('click', closeAllMenus);
+headerCloseButton?.addEventListener("click", closeAllMenus);
+body.classList.remove(notScroll);
 
-function closeAllMenus(){
-    // Mostrar botones
-    headerBurgerButton.classList.remove(hiddenClass);
-    headerProfileButton.classList.remove(hiddenClass);
-    // Ocultar botón de cierre
-    headerCloseButton.classList.add(hiddenClass);
-    // Ocultar menús desplegables
-    headerProfileMenu?.classList.remove(classToggle);
-    headerBurgerMenu?.classList.remove(classToggle);
-    headerLanguageMenu?.classList.remove(classToggle);
-    // Desbloquear scroll
-    body.classList.remove(notScroll);
+function closeAllMenus() {
+  // Mostrar botones
+  headerBurgerButton.classList.remove(hiddenClass);
+  headerProfileButton.classList.remove(hiddenClass);
+  // Ocultar botón de cierre
+  headerCloseButton.classList.add(hiddenClass);
+  // Ocultar menús desplegables
+  headerProfileMenu?.classList.remove(classToggle);
+  headerBurgerMenu?.classList.remove(classToggle);
+  headerLanguageMenu?.classList.remove(classToggle);
+  // Desbloquear scroll
+  body.classList.remove(notScroll);
 }
 
 // Función para mostrar/ocultar el submenu del profile
 //---------------------------------------------------------------------------------
-headerProfileButton?.addEventListener('click', showProfileMobileSubmenu);
+headerProfileButton?.addEventListener("click", showProfileMobileSubmenu);
 
-function showProfileMobileSubmenu(){
-    // Mostrar menú
-    headerProfileMenu?.classList.add(classToggle);
+function showProfileMobileSubmenu() {
+  // Mostrar menú
+  headerProfileMenu?.classList.add(classToggle);
 
-    // Cambiar botones disponibles
-    headerProfileButton.classList.add(hiddenClass);
-    headerBurgerButton.classList.add(hiddenClass);
-    headerCloseButton.classList.remove(hiddenClass);
-    // Bloquear scroll
-    body.classList.add(notScroll);
+  // Cambiar botones disponibles
+  headerProfileButton.classList.add(hiddenClass);
+  headerBurgerButton.classList.add(hiddenClass);
+  headerCloseButton.classList.remove(hiddenClass);
+  // Bloquear scroll
+  body.classList.add(notScroll);
 }
 
 // Función para mostrar/ocultar el submenu burger
 //---------------------------------------------------------------------------------
-headerBurgerButton?.addEventListener('click', showBurgerMobileSubmenu);
+headerBurgerButton?.addEventListener("click", showBurgerMobileSubmenu);
 
-function showBurgerMobileSubmenu(){
-    // Mostrar menú
-    headerBurgerMenu?.classList.add(classToggle);
+function showBurgerMobileSubmenu() {
+  // Mostrar menú
+  headerBurgerMenu?.classList.add(classToggle);
 
-    // Cambiar botones disponibles
-    headerProfileButton.classList.add(hiddenClass);
-    headerBurgerButton.classList.add(hiddenClass);
-    headerCloseButton.classList.remove(hiddenClass);
-    // Bloquear scroll
-    body.classList.add(notScroll);
+  // Cambiar botones disponibles
+  headerProfileButton.classList.add(hiddenClass);
+  headerBurgerButton.classList.add(hiddenClass);
+  headerCloseButton.classList.remove(hiddenClass);
+  // Bloquear scroll
+  body.classList.add(notScroll);
 }
 
 // Función para mostrar/ocultar el submenu language
 //---------------------------------------------------------------------------------
-headerLanguageButton?.addEventListener('click', showLanguageMobileSubmenu);
+headerLanguageButton?.addEventListener("click", showLanguageMobileSubmenu);
 
-function showLanguageMobileSubmenu(){
-    // Mostrar menú
-    headerLanguageMenu?.classList.toggle(classToggle);
+function showLanguageMobileSubmenu() {
+  // Mostrar menú
+  headerLanguageMenu?.classList.toggle(classToggle);
 }
